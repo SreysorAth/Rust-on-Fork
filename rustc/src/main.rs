7c57f6d435e8661cdf35e09ffb5000e31fa1c6a0
SreysorAth <147776796+SreysorAth@users.noreply.github.com>
2023-11-07 22:07:36 +0700

    Update main.rs

--- rustc/src/main.rs
+++ rustc/src/main.rs
@@ -1,12 +1,21 @@
-
 extern crate libc;
 use std::process;
-
+// /home/sreysor-ath/POSIX syscall/rustc/target/debug 
+//  cd '/home/sreysor-ath/POSIX syscall/rustc/target/debug'
+// strace ./rustc
 const SIZE: usize = 5;
-
+//let mut nums: [i32; SIZE] = [0, 1, 2, 3, 4];
+//static mut nums: &'static [i32] = &[0, 1, 2, 3, 4];
+static mut NUMS: [i32; SIZE] = [0, 1, 2, 3, 4];
+//cargo build
 fn main() {
-    let mut nums: [i32; SIZE] = [0, 1, 2, 3, 4];
+
+  
+ //let pid = 30;
     let pid = unsafe { libc::fork() };
+
+unsafe  {
+    println!("rust : after fork  pid = {}", pid ); 
 
     if pid < 0 {
         println!("Fork failed");
@@ -14,17 +23,18 @@ fn main() {
     } else if pid == 0 {
         // Child process
         for i in 0..SIZE {
-            nums[i] *= -(i as i32);
-            println!("CHILD: {}", nums[i]);
+            NUMS[i] *= -(i as i32);
+            println!("CHILD: {}, pid = {}" , NUMS[i],pid);
         }
     } else {
         // Parent process
-        unsafe {
+       // unsafe {
             let mut status: i32 = 0;
-            //libc::waitpid(pid, &mut status, 0);
-        }
-        for i in 0..SIZE {
-            println!("PARENT: {}", nums[i]);
-        }
+            libc::waitpid(pid, &mut status, 0);
+            for i in 0..SIZE {
+                println!("PARENT: {}, pid = {}", NUMS[i], pid);
+            }
+       // }
     }
+}
 }
